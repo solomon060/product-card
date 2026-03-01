18b0d8962ade36eef7e966668bf0d294637ac0ac
Sergei <solomon06@yandex,ru>
2026-03-01 20:20:31 +0300

    fix errors

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Покраска всех карточек
 
 const productCards = document.querySelectorAll('.card-container');
-const changeColorAllCardButton = document.querySelector('#chenge-color-all-card');
+const changeColorAllCardButton = document.querySelector('#change-color-all-card');
 const greenColorHash = '#a0cea0ff';
 
 changeColorAllCardButton.addEventListener('click', () => {
@@ -11,7 +11,7 @@ changeColorAllCardButton.addEventListener('click', () => {
 // Покраска первой карточки
 
 const firstProductCard = document.querySelector('.card-container');
-const changeColorFirstCardButton = document.querySelector('#chenge-color-first-card');
+const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
 const blueColorHash = '#8b9bccff';
 
 changeColorFirstCardButton.addEventListener('click', () => {
@@ -36,9 +36,9 @@ function openGoogle() {
 const outputLogButton = document.querySelector('#output-console-log');
 outputLogButton.addEventListener('click', () => outputConsoleLog('ДЗ №4'));
 
-function outputConsoleLog(massage) {
-  alert(massage)
-  console.log(massage)
+function outputConsoleLog(message) {
+  alert(message)
+  console.log(message)
 }
 
 // Вывод названия в лог при наведении мыши
@@ -54,11 +54,5 @@ titleLog.addEventListener('mouseenter', () => {
 const colorButton = document.getElementById('change-color-button');
 
 colorButton.addEventListener('click', () => {
-  colorButton.classList.toggle('active-color');
-
-  if (!colorButton.classList.contains('active-color')) {
-    colorButton.style.backgroundColor = '#db990bff';
-  } else {
-    colorButton.style.backgroundColor = '#1da9c5ff';
-  }
+  colorButton.classList.toggle('active-color'); 
 });
